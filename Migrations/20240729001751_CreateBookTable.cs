ca52d3ad51aae79aa37e28eb82e8dcb5e38128ff
artur <artur.oliveirasantiago@gmail.com>
2024-07-28 23:33:50 -0300

    chore: Add create book table `Migration`

---
+++ Migrations/20240729001751_CreateBookTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace books_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateBookTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE TABLE books (
+	            id uuid NOT NULL,
+	            title text NOT NULL,
+	            CONSTRAINT PK_books PRIMARY KEY (id)
+            );");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TABLE books;");
+
+        }
+    }
+}
