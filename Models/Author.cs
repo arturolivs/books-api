75b00ad7c01c37a639984400dab380b39ca6b6c8
artur <artur.oliveirasantiago@gmail.com>
2024-07-31 17:52:02 -0300

    feat: Add `Author`  class and `migrations`

---
+++ Models/Author.cs
@@ -0,0 +1,45 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    [Table("authors")]
+    public class Author
+    {
+        [Column("id")]
+        public Guid Id { get; init; }
+
+        [Column("first_name")]
+        public string FirstName { get; private set; }
+
+        [Column("last_name")]
+        public string LastName { get; private set; }
+
+        [Column("birth_date")]
+        public DateTime BirthDate { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+
+        public Author(string firstName, string lastName, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+
+        public void SetFirstName(string firstName)
+        {
+            FirstName = firstName;
+        }
+
+        public void SetLastName(string lastName)
+        {
+            LastName = lastName;
+        }
+
+        public void SetBirthDate(DateTime birthDate)
+        {
+            BirthDate = birthDate;
+        }
+    }
+}
