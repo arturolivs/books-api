f59e106fc8070217d8f367a4f0f379bb90aca11c
artur <artur.oliveirasantiago@gmail.com>
2024-07-31 14:57:15 -0300

    feat: Add `migration` to create `genres` table

---
+++ Migrations/20240731175411_CreateGenresTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace books_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateGenresTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE TABLE genres (
+	            id uuid NOT NULL,
+	            name text NOT NULL,
+	            CONSTRAINT PK_genres PRIMARY KEY (id)
+            );");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TABLE genres;");
+
+        }
+    }
+}
